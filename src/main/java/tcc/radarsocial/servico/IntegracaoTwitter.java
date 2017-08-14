490e260823b59ca248b5fe9aee91d401ca834f92
7-PC\7 <patriciasoramos@gmail.com>
2017-08-14 19:10:23 -0300

    Commit test

--- src/main/java/tcc/radarsocial/servico/IntegracaoTwitter.java
+++ src/main/java/tcc/radarsocial/servico/IntegracaoTwitter.java
@@ -50,9 +50,12 @@ public class IntegracaoTwitter {
 			tweet.setFavorites(st.getFavoriteCount());
 			tweet.setDataCriacao(st.getCreatedAt());
 			
-			if(st.getMediaEntities().length!=0)
+			if(st.getMediaEntities().length!=0){
+//				System.out.println(st.getMediaEntities()[0].getMediaURL());
 				tweet.setImagem(st.getMediaEntities()[0].getMediaURL());
-			
+			}else{
+				tweet.setImagem(null);
+			}
 			tweet.setLink("https://twitter.com/"+twitterProfile+"/status/"+st.getId());	
 			
 			TwitterDao dao = new TwitterDao();
