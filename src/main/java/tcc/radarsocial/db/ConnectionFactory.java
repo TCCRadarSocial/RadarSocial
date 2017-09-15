77a6c83b2643900c2a8aa032cc1f623891e2a135
idea-PC\Patricia <patriciasoramos@gmail.com>
2017-09-14 23:33:14 -0300

    Adicionada nova dependencia para corrigir problema no mongodb

--- src/main/java/tcc/radarsocial/db/ConnectionFactory.java
+++ src/main/java/tcc/radarsocial/db/ConnectionFactory.java
@@ -5,25 +5,40 @@ import java.net.UnknownHostException;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
+//import com.mongodb.client.MongoDatabase;
 
 public class ConnectionFactory {
+	
+//	public static DB db = null;
+//	public static MongoDatabase database = null;
 
 	public static DB connectDB(){
 		DB db = null;
 		
 		try{
 			
-		Mongo mongo = new Mongo("localhost", 27017);
-		db = mongo.getDB("RadarSocial");
+//			MongoClient client = new MongoClient("localhost", 27017);
+//			database = client.getDatabase("RadarSocial");
+//			client.close();
+//		if(db == null){
+			
+			
+			
+			Mongo mongo = new Mongo("localhost", 27017);
+			db = mongo.getDB("RadarSocial");
+//		}
 	
 		//DBCollection collection = db.getCollection("dummyColl");
 		
-		}catch (UnknownHostException e) {
-			e.printStackTrace();
-	    } catch (MongoException erro) {
+		}catch (MongoException erro) {
 	    	erro.printStackTrace();
-	    }
+	    	
+	    } catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return db;
 	}
