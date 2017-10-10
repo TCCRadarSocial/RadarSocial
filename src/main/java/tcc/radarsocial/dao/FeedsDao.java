633b1037d3315084d452b4dee5ddc3392f721c96
idea-PC\Patricia <patriciasoramos@gmail.com>
2017-10-09 23:42:02 -0300

    Criada comparação entre redes sociais

--- src/main/java/tcc/radarsocial/dao/FeedsDao.java
+++ src/main/java/tcc/radarsocial/dao/FeedsDao.java
@@ -2,6 +2,7 @@ package tcc.radarsocial.dao;
 
 import java.text.ParseException;
 
+import com.mongodb.AggregationOutput;
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
@@ -15,6 +16,8 @@ public class FeedsDao {
 
 
 	DBCollection collection = ConnectionFactory.connectDB().getCollection("FeedsMetricas");
+//	DBCollection collectionFace = ConnectionFactory.connectDB().getCollection("FacebookMetricas");
+//	DBCollection collectionTwitter = ConnectionFactory.connectDB().getCollection("TwitterMetricas");
 	
 	public DBCursor buscaPorFiltro(String portal, String dataInicial, String dataFinal,String redeSocial, String orderBy, String palavraChave) throws ParseException{
 		
@@ -92,5 +95,69 @@ public class FeedsDao {
 		collection.remove(query);
 		
 	}
+	
+	
+	public String buscarComparativoAgregacaoRedes(String portalFacebook,String portalTwitter,String dataInicial,String dataFinal){
+		
+		BasicDBObject matchFacebook = null;
+		BasicDBObject matchTwitter = null;
+		
+		if(!portalFacebook.isEmpty()){
+			
+			
+			matchFacebook = (BasicDBObject) JSON.parse("{ \"$match\": {\"$and\": ["
+					+ "{ \"dataCriacao\" : { \"$gte\" : { \"$date\" : \""+dataInicial+"\"} , \"$lte\" : { \"$date\" : \""+dataFinal+"\"}} },{\"nomePagina\": \""+portalFacebook+"\"}]}}");
+		
+//			matchFacebook = (BasicDBObject) JSON.parse("{ \"$match\": {\"nomePagina\": \""+portalFacebook+"\"}}");
+			
+			
+			}
+		
+		if(!portalTwitter.isEmpty()){
+//			matchTwitter = (BasicDBObject) JSON.parse("{ \"$match\": {\"nomeTwitter\": \""+portalTwitter+"\"}}");
+			matchTwitter = (BasicDBObject) JSON.parse("{ \"$match\": {\"$and\": ["
+					+ "{ \"dataCriacao\" : { \"$gte\" : { \"$date\" : \""+dataInicial+"\"} , \"$lte\" : { \"$date\" : \""+dataFinal+"\"}} },{\"nomeTwitter\": \""+portalTwitter+"\"}]}}");
+		
+		}		
+		
+		// $group operation
+		BasicDBObject groupFieldsFacebook = new BasicDBObject( "_id", "$dataCriacao");
+		groupFieldsFacebook.put("sum_reactions", new BasicDBObject( "$sum", "$reactions"));
+
+		BasicDBObject groupFieldsTwitter = new BasicDBObject( "_id", "$dataCriacao");
+		groupFieldsTwitter.put("sum_retweets", new BasicDBObject( "$sum", "$retweets"));
+		
+		
+		BasicDBObject groupFace = new BasicDBObject("$group", groupFieldsFacebook);
+		BasicDBObject groupTwitter = new BasicDBObject("$group", groupFieldsTwitter);
+		
+		//sort
+		DBObject sortFields = new BasicDBObject("dataCriacao", 1);
+		DBObject sort = new BasicDBObject("$sort", sortFields );
+		
+		// run aggregation
+		AggregationOutput outputFace = collection.aggregate(matchFacebook, groupFace,sort);
+		AggregationOutput outputTwitter = collection.aggregate(matchTwitter, groupTwitter,sort);
+		
+		
+		String jsonTwitter = null;
+		String jsonFace = null;
+		
+		if(!outputTwitter.results().toString().equals(""))
+			jsonTwitter =  outputTwitter.results().toString().replaceAll("\\[+\\]+","");
+		
+		if(!outputFace.results().toString().equals(""))
+			jsonFace = outputFace.results().toString().replaceAll("\\[+\\]+","");
+		
+		String serialize = null;
+		if(!jsonFace.equals("") && !jsonTwitter.equals(""))
+			serialize = "[" + jsonTwitter + ", " + jsonFace + "]";
+		else
+			serialize = "";
+			
+		return serialize;
+		
+	}
+	
 	
 }
